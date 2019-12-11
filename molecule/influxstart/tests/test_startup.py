136be79245e0afb2a9ebe125a35d6d9f84c93266
Travis Holton <travis@catalyst.net.nz>
2019-12-11 16:56:15 +1300

    clean up lint errors in tests

--- molecule/influxstart/tests/test_startup.py
+++ molecule/influxstart/tests/test_startup.py
@@ -1,11 +1,9 @@
 import os
-
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
-
 
 
 def test_influx_service_enabled(host):
